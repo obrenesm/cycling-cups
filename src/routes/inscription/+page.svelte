22b4c30dd0ad105dfd5c19d4f3b7e2da046acc08
Oscar Brenes <oscar.brenes@hangarww.com>
2023-02-21 07:58:21 -0600

    adding inputs

--- src/routes/inscription/+page.svelte
+++ src/routes/inscription/+page.svelte
@@ -1,32 +1,88 @@
 <script lang="ts">
-    let yolo = "you only live once"
 </script>
 
 
-<h1>Inscriptions</h1>
+<h1>Inscripción</h1>
+
+<form>
+    <!-- <div class="grid"> -->
+        <label>Licencia de Corredor
+            <input class="license" placeholder="Número de Licencia" type="text">
+        </label>
+        <label>Evento
+            <select class="event">
+                <optgroup>
+                    <option value="" selected>Select --</option>
+                    <option value="copa-mtb">Copa Nacional MTB</option>
+                    <option value="crc">Costa Rica</option>
+                    <option value="nic">Nicaragua</option>
+                </optgroup>
+            </select>
+        </label>
+        <label>Referencia de Pago
+            <input class="referencia" placeholder="Número de Referencia" type="text">
+        </label>
+        <button type="submit">Inscribir</button>
+    <!-- </div> -->
+    <!-- <div class="grid">
+        <label>Nombre
+            <input class="name" placeholder="Nombre" type="text">
+        </label>
+        <label>Apellidos
+            <input class="lastname" placeholder="Apellido" type="text">
+        </label>
+    </div>
+    <div class="grid">
+        <label>Cédula
+            <input class="personalId" placeholder="Cédula" type="text">
+        </label>
+        <label>Nacionalidad
+        <select class="nationality">
+            <optgroup>
+                <option value="" selected>Select --</option>
+                <option value="usa">United States</option>
+                <option value="crc">Costa Rica</option>
+                <option value="nic">Nicaragua</option>
+            </optgroup>
+        </select>
+        </label>
+        <label for="date">Date
+            <input type="date" id="date" name="date">
+        </label>
+    </div>
+    <label>Categoría
+        <select class="category">
+            <optgroup>
+                <option value="" selected>Select --</option>
+                <option value="kids">Niños</option>
+                <option value="kids-fem">Niñas</option>
+                <option value="infant">Infantil</option>
+                <option value="infant-fem">Infantil Femenino</option>
+                <option value="prejr">Pre Juvenil</option>
+                <option value="prejr-fem">Pre Juvenil Femenino</option>
+                <option value="jr">Juvenil</option>
+                <option value="jr-fem">Juvenil Femenino</option>
+                <option value="u23">Sub 23</option>
+                <option value="u23">Sub 23 Femenino</option>
+                <option value="elite">Elite</option>
+                <option value="elite-fem">Elite Femenino</option>
+                <option value="mastera">Master A</option>
+                <option value="masterb">Master B</option>
+                <option value="masterc">Master C</option>
+                <option value="master-fem">Master Femenino</option>
+                <option value="ebike">E-Bike</option>
+            </optgroup>
+        </select>
+    </label> -->
+</form>
+
 <p>Visit <a href="/">home</a> sweat home</p>
 
 
-<p>{yolo}</p>
 
-<form>
-    <div class="grid">
-        <input class="name" type="text">
-        <input class="lastname" type="text">
-        <input class="personalId" type="text">
-        <select class="nationality">
-            <option value="null">Select --</option>
-            <option value="usa">United States</option>
-            <option value="crc">Costa Rica</option>
-            <option value="nic">Nicaragua</option>
-        </select>
-    </div>
-    <select class="category">
-        <optgroup>
-            <option value="null">Select --</option>
-            <option value="kids">Niños</option>
-            <option value="infant">Infantil</option>
-            <option value="nic">Nicaragua</option>
-        </optgroup>
-    </select>
-</form>
+<style>
+	form {
+        max-width: 500px;
+        margin: 0 auto;
+    }
+</style>
