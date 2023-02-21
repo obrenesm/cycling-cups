35caac5186e92b3842c34fcefc6af46ed0494544
Oscar Brenes <oscar.brenes@hangarww.com>
2023-02-20 18:53:42 -0600

    opening repository for the project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import "@picocss/pico/css/pico.css"
+</script>
+<h1>Bye to SvelteKit</h1>
+<p>Make an <a href="/inscription">inscription</a></p>
+
---
+++ src/routes/inscription/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    let yolo = "you only live once"
+</script>
+
+
+<h1>Inscriptions</h1>
+<p>Visit <a href="/">home</a> sweat home</p>
+
+
+<p>{yolo}</p>
+
+<form>
+    <div class="grid">
+        <input class="name" type="text">
+        <input class="lastname" type="text">
+        <input class="personalId" type="text">
+        <select class="nationality">
+            <option value="null">Select --</option>
+            <option value="usa">United States</option>
+            <option value="crc">Costa Rica</option>
+            <option value="nic">Nicaragua</option>
+        </select>
+    </div>
+    <select class="category">
+        <optgroup>
+            <option value="null">Select --</option>
+            <option value="kids">Niños</option>
+            <option value="infant">Infantil</option>
+            <option value="nic">Nicaragua</option>
+        </optgroup>
+    </select>
+</form>
